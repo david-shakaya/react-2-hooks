5468affe7e00fc9c5504ca411f09d586c07986ed
david-shakaya <devidoz666@gmail.com>
2021-03-10 15:18:50 +0200

    Добавл counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,18 +2,32 @@ import React from "react";
 import style from "./Counter.module.css";
 
 class Counter extends React.Component {
+  state = {
+    value: 1,
+    defaultValue: 0,
+  };
+
   handleIncrement = () => {
-    console.log("+1");
+    // Если нужно использовать event в асинхронном коде. Сохраням евент в переменную и используем переменную
+    this.setState((prevState) => {
+      return { value: prevState.value + 1 };
+    });
   };
 
   handleDecrement = () => {
-    console.log("-1");
+    this.setState((prevState) => {
+      return {
+        value:
+          prevState.value === 0 ? (prevState.value = 0) : prevState.value - 1,
+        //   если значение равно нулю верни ноль, если больше верни -1
+      };
+    });
   };
 
   render() {
     return (
       <div className={style.Counter__wrapper}>
-        <span className={style.Span}>1</span>
+        <span className={style.Span}>{this.state.value}</span>
         <div className={style.Counter__controls}>
           <button type="button" onClick={this.handleIncrement}>
             Увеличить на 1
